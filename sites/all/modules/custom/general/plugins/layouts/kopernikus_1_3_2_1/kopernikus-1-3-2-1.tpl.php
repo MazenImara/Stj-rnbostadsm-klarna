799e6ef22bfa864049ccb1079de8c3d73fd17bdf
Kristofer Tengström <kristofer@storleden.se>
2015-05-27 18:11:24 +0200

    kristofer: Working HTML for Pure White

---
+++ sites/all/modules/custom/general/plugins/layouts/kopernikus_1_3_2_1/kopernikus-1-3-2-1.tpl.php
@@ -0,0 +1,23 @@
+<div class="panel-display omega-grid kopernikus-1-3-2-1" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-24 top">
+    <div class="inside"><?php print $content['top']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 left">
+    <div class="inside"><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 middle">
+    <div class="inside"><?php print $content['middle']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 right">
+    <div class="inside"><?php print $content['right']; ?></div>
+  </div>
+  <div class="panel-panel grid-10 bottom bottom-1">
+    <div class="inside"><?php print $content['bottom_1']; ?></div>
+  </div>
+  <div class="panel-panel grid-14 bottom bottom-2">
+    <div class="inside"><?php print $content['bottom_2']; ?></div>
+  </div>
+  <div class="panel-panel grid-24 footer">
+    <div class="inside"><?php print $content['footer']; ?></div>
+  </div>
+</div>

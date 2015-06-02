ff170adc176596aad6f62fee1b4ee513439e3914
Kristofer Tengström <kristofer@storleden.se>
2015-06-02 14:49:38 +0200

    kristofer: Added panels layout for hero

---
+++ sites/all/modules/custom/general/plugins/layouts/kopernikus_1_3_3_1/kopernikus-1-3-3-1.tpl.php
@@ -0,0 +1,26 @@
+<div class="panel-display omega-grid kopernikus-1-3-3-1" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-24 header">
+    <div class="inside"><?php print $content['header']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 top top-left">
+    <div class="inside"><?php print $content['top_left']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 top top-middle">
+    <div class="inside"><?php print $content['top_middle']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 top top-right">
+    <div class="inside"><?php print $content['top_right']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 bottom bottom-left">
+    <div class="inside"><?php print $content['bottom_left']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 bottom bottom-middle">
+    <div class="inside"><?php print $content['bottom_middle']; ?></div>
+  </div>
+  <div class="panel-panel grid-8 bottom bottom-right">
+    <div class="inside"><?php print $content['bottom_right']; ?></div>
+  </div>
+  <div class="panel-panel grid-24 footer">
+    <div class="inside"><?php print $content['footer']; ?></div>
+  </div>
+</div>

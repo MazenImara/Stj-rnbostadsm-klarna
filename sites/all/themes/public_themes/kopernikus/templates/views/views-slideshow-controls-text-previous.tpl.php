74cde011df5d7bb4e9a4fed56ed449d7e2d2f174
Kristofer Tengström <kristofer@storleden.se>
2015-06-03 18:05:57 +0200

    kristofer: Added login block. Removed debug code

--- sites/all/themes/public_themes/kopernikus/templates/views/views-slideshow-controls-text-previous.tpl.php
+++ sites/all/themes/public_themes/kopernikus/templates/views/views-slideshow-controls-text-previous.tpl.php
@@ -1,2 +1,1 @@
-<?php drupal_set_message("text-previous"); ?>
 <span id="views_slideshow_controls_text_previous_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"></a></span>
